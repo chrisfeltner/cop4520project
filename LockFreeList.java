f14e10c825b66b91c8435c7d5a9174b32482e0d2
Jesse <jesserehrer@knights.ucf.edu>
2021-03-07 21:07:19 -0500

    Finished delete for LockFreeList tested with Runner. Used while (true) but can optimize later.

--- LockFreeList.java
+++ LockFreeList.java
@@ -126,6 +126,111 @@ public class LockFreeList {
     }
   }
 
+  /**
+   * Delete the node with specified key if it exists.
+   * 
+   * @param keyToDelete The key of the node we are trying to delete
+   * @return True if successful
+   */
+  public boolean delete(int keyToDelete) {
+    Node prev = this.head;
+    Node current;
+    Node succ;
+    boolean[] markHolder = { false };
+    while (true) {
+      // Perform the same traversal as find but stop once node to delete is found
+      while (true) {
+        current = prev.next.get(markHolder);
+        // Condition 1: We reach the end of the list without finding key
+        if (current == null) {
+          return false;
+        }
+        boolean currentMark = markHolder[0];
+        int currentKey = current.key;
+        // Condition 2: The previous node is marked or is no longer the previous node
+        if (prev.next.get(markHolder) != current || markHolder[0] == true) {
+          // Start over
+          delete(keyToDelete);
+        }
+        // The current node is not marked (not deleted)
+        if (!currentMark) {
+          if (currentKey == keyToDelete) {
+            break;
+          } else if (currentKey > keyToDelete) {
+            return false;
+          } else {
+            prev = current.next.get(markHolder);
+          }
+        }
+        prev = current;
+      }
+
+      succ = current.next.getReference();
+      // Try to perform logical deletion, if we can't then try again
+      if (!current.next.compareAndSet(succ, succ, false, true)) {
+        continue;
+      }
+      // Try to perform physical deletion, if we can't then find will
+      if (prev.next.compareAndSet(current, succ, false, false)) {
+        deleteNode(current);
+      }
+      return true;
+    }
+  }
+
+  /**
+   * Try to delete the node with specified key from the list starting at the
+   * specified starting point (used for deletion from hash table using shortcut
+   * references).
+   * 
+   * @param startingPoint Node reference where we will begin our traversal
+   * @param keyToDelete   The key of the node we are trying to delete
+   * @return True if successful
+   */
+  public boolean deleteAfter(Node startingPoint, int keyToDelete) {
+    Node prev = startingPoint;
+    Node current;
+    Node succ;
+    boolean[] markHolder = { false };
+    while (true) {
+      // Perform the same traversal as find but stop once node to delete is found
+      while (true) {
+        current = prev.next.get(markHolder);
+        // Condition 1: We reach the end of the list without finding key
+        if (current == null) {
+          return false;
+        }
+        boolean currentMark = markHolder[0];
+        int currentKey = current.key;
+        // Condition 2: The previous node is marked or is no longer the previous node
+        if (prev.next.get(markHolder) != current || markHolder[0] == true) {
+          // Start over
+          delete(keyToDelete);
+        }
+        // The current node is not marked (not deleted)
+        if (!currentMark) {
+          if (currentKey == keyToDelete) {
+            break;
+          } else if (currentKey > keyToDelete) {
+            return false;
+          } else {
+            prev = current.next.get(markHolder);
+          }
+        }
+        prev = current;
+      }
+
+      succ = current.next.getReference();
+      if (!current.next.compareAndSet(succ, succ, false, true)) {
+        continue;
+      }
+      if (prev.next.compareAndSet(current, succ, false, false)) {
+        deleteNode(current);
+      }
+      return true;
+    }
+  }
+
   private void deleteNode(Node node) {
     node.key = 0;
     node.next = null;
--- Runner.java
+++ Runner.java
@@ -8,13 +8,20 @@ public class Runner {
   public static void main(String[] args) {
     LockFreeList list = new LockFreeList();
     System.out.println(list.toString());
-    list.insert(new Node(10, null));
+    list.insert(new Node(30, null));
     System.out.println(list.toString());
     System.out.println(list.find(10));
     System.out.println(list.find(20));
     list.insert(new Node(20, null));
-    list.insert(new Node(30, null));
+    list.insert(new Node(10, null));
+    System.out.println(list.toString());
+    list.delete(30);
+    System.out.println(list.toString());
+    list.delete(10);
+    System.out.println(list.toString());
+    list.delete(20);
+    System.out.println(list.toString());
+    list.delete(50);
     System.out.println(list.toString());
   }
-
 }
